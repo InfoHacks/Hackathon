41e51feb94851fec97767926b27bea57ce684a27
William Cohen <William.Cohen@infotrack.com.au>
2022-11-09 14:05:21 +1100

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
